f814476cd239a31fc7057e8638221b2fcf603f3d
L1mbxbx <ilya.msd2003@gmail.com>
2025-07-25 14:50:23 +0300

    Fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 kind delete cluster
 kind create cluster --config cluster.yml
 
-kubectl taint nodes kind-worker kind-worker2 app=mysql:NoSchedule
+kubectl taint nodes kind-worker app=mysql:NoSchedule
+kubectl taint nodes kind-worker2 app=mysql:NoSchedule
 
 helm install todoapp helm-chart/todoapp/
 helm get all todoapp
